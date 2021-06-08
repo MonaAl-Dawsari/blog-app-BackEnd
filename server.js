77765000df8a60f7c53d8274fdf8eb08c7e5c009
Alsaiqal <mona.alsaiqal@live.co.uk>
2021-06-08 09:52:56 +0300

    comment added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const categoryRoute = require("./routes/categories");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-// const commentRoute = require("./routes/comment");
+const commentRoute = require("./routes/comment");
 const multer = require("multer");//multer for upload files
 const path = require("path");
 
@@ -64,7 +64,8 @@ app.use("/auth", authRoute);
 app.use("/users", userRoute);
 app.use("/blog/v1/posts", postRoute);
 app.use("/blog/v1/categories", categoryRoute);
-// app.use("/comment", commentRoute);
+app.use("/comment", commentRoute);
+
 
 // Listen for HTTP request PORT
 app.listen(PORT, () => {
