0ae5d1b0004e30a0f35d5d8a7814796826658385
MonaAldaw <monaaldosari16@gmail.com>
2021-06-12 16:05:00 +0300

    Done With pictures

--- models/Post.js
+++ models/Post.js
@@ -12,7 +12,7 @@ const PostSchema = new mongoose.Schema(
     },
     photo: {
       type: String,
-      default: "photo-1501504905252-473c47e087f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+      default: "1623497349113blogpic.jpg"
       
     },
     username: {//to know who is the auther for the post
